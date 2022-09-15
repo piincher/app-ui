46991f5f5c0105a9b1dd51ca9614c04f306131ef
Ibrahim <ikouma269@gmail.com>
2022-09-16 07:33:13 +0800

    completed

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const App = express();
+const cors = require("cors");
 
+App.use(cors());
+
+App.use(express.json());
 App.get("/", (req, res) => {
   res.send("hello from the server");
+});
+
+App.post("/api/state/cache", (req, res) => {
+  try {
+    console.log(req.body);
+    const data = req.body;
+
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
 const PORT = 4000;
 App.listen(PORT, () => {
